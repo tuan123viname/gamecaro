859120038b60098a7a197280dae559e5c5a4842a
ThaiAn07 <51089427+ThaiAn07@users.noreply.github.com>
2019-06-10 12:59:43 +0700

    :)

---
+++ game Caro deadline 31/game Caro deadline 31/Accept.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace game_Caro_deadline_31
+{
+    public partial class Accept : Form
+    {
+        public Accept()
+        {
+            InitializeComponent();
+        }
+    }
+}
